f9afc8dd1333b900a9049571780e401356878854
s.kim <s.kim@bizreach.co.jp>
2020-05-18 22:32:01 +0900

    HTTP responseにヘッダーをつけて返す

--- java-simple-http-server/SimpleHttpServer.java
+++ java-simple-http-server/SimpleHttpServer.java
@@ -1,14 +1,62 @@
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class SimpleHttpServer {
+
+    private static int getLength(byte[] b){
+        int count = 0;
+        for(byte b_ : b){
+            if(b_ == (byte)0) break;
+            ++count;
+        }
+        return count;
+    }
+
+    private static byte[] readFrom(InputStream inputStream, int length) throws IOException {
+        byte[] bytes = new byte[length];
+
+        inputStream.read(bytes);
+        return bytes;
+    }
+
+    private static String getDateFromHttpFormat(Date date){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, dd MMM YYYY HH:mm:ss z", Locale.ENGLISH);
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return simpleDateFormat.format(date);
+    }
+
+    private static void writeTo(OutputStream outputStream, byte[] b) throws IOException {
+        outputStream.write("HTTP/1.1 200 OK\r\n".getBytes());
+        // Date
+        outputStream.write("Date: ".getBytes());
+        outputStream.write(getDateFromHttpFormat(new Date()).getBytes());
+        outputStream.write("\r\n".getBytes());
+        // Content length
+        outputStream.write("Content-Length: ".getBytes());
+        outputStream.write(Integer.toString(getLength(b)).getBytes());
+        outputStream.write("\r\n".getBytes());
+        // Connection
+        outputStream.write("Connection: ".getBytes());
+        outputStream.write("Close".getBytes());
+        outputStream.write("\r\n".getBytes());
+        // CRLF
+        outputStream.write("\r\n".getBytes());
+        // body
+        outputStream.write(b);
+    }
 
     public static void main(String[] args) {
 
         // get port number from args
-        int port = Integer.parseInt(args[0]);
+        int port = 8000;
         Socket socket = null;
         InputStream inputStream = null;
         OutputStream outputStream = null;
@@ -28,14 +76,8 @@ public class SimpleHttpServer {
                 inputStream = socket.getInputStream();
                 outputStream = socket.getOutputStream();
 
-                byte[] bytes = new byte[1024];
-
-                inputStream.read(bytes);
-
-                System.out.print(new String(bytes));
-
-                // if conneteced, write hello world
-                outputStream.write(bytes);
+                byte[] b = readFrom(inputStream,1024);
+                writeTo(outputStream , b);
 
                 socket.close();
                 inputStream.close();
